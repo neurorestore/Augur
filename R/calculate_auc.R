494100733b089039b6da7a4ffbf8cb429faab311
skinnider <michaelskinnider@gmail.com>
2021-04-21 12:58:19 -0700

    force Augur to run on counts for Seurat objects

--- R/calculate_auc.R
+++ R/calculate_auc.R
@@ -178,7 +178,7 @@ calculate_auc = function(input,
       droplevels()
     cell_types = meta[[cell_type_col]]
     labels = meta[[label_col]]
-    expr = Seurat::GetAssayData(input)
+    expr = Seurat::GetAssayData(input, slot = 'counts')
 
     # print default assay
     default_assay = Seurat::DefaultAssay(input)
